f2f0fcf2f404913558fc72410af55d8e4576322f
e-Paul0 <130754722+e-Paul0@users.noreply.github.com>
2023-05-25 22:15:23 +0100

    Conversor7Amb

--- jaCOMPLETOS/AmbienteBase/04.Conversor7seg.vhdl
+++ jaCOMPLETOS/AmbienteBase/04.Conversor7seg.vhdl
@@ -5,11 +5,12 @@ entity Conv7Amb is
 
 	port (inPort0  	: in std_logic_vector(3 downto 0);
 			inPort1  	: in std_logic_vector(3 downto 0);
-			inPort3  	: in std_logic_vector(3 downto 0);
+			inPort2  	: in std_logic_vector(3 downto 0);
 	
 			outPort0		: out std_logic_vector(6 downto 0);
 			outPort1		: out std_logic_vector(6 downto 0);
-			outPort2		: out std_logic_vector(6 downto 0);
+			outPort2		: out std_logic_vector(6 downto 0));
+			
 end Conv7Amb;
 
 architecture compConv7Amb of Conv7Amb is
@@ -36,17 +37,16 @@ begin
 					  "0000000" when (inPort1 	= "1000") else --8
 					  "0010000" when (inPort1 	= "1001") else --9
 					  "1000000";
-					  			  
-		outPort2 <= "1111001" when (inPort3 = "0001") else --1
-					  "0100100" when (inPort3 	= "0010") else --2
-					  "0110000" when (inPort3 	= "0011") else --3
-					  "0011001" when (inPort3 	= "0100") else --4
-					  "0010010" when (inPort3 	= "0101") else --5
-					  "0000010" when (inPort3 	= "0110") else --6
-					  "1111000" when (inPort3 	= "0111") else --7
-					  "0000000" when (inPort3 	= "1000") else --8
-					  "0010000" when (inPort3 	= "1001") else --9
-					  "1000000";
-						  
+					  
+		outPort2 <= "1111001" when (inPort2 = "0001") else --1
+					  "0100100" when (inPort2 	= "0010") else --2
+					  "0110000" when (inPort2 	= "0011") else --3
+					  "0011001" when (inPort2 	= "0100") else --4
+					  "0010010" when (inPort2 	= "0101") else --5
+					  "0000010" when (inPort2 	= "0110") else --6
+					  "1111000" when (inPort2 	= "0111") else --7
+					  "0000000" when (inPort2 	= "1000") else --8
+					  "0010000" when (inPort2 	= "1001") else --9
+					  "1000000";			  
 			
 end compConv7Amb;			
